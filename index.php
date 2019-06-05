4623d2b6391cfaaa66854da0fe5b9606646dcc71
ilan zilcha <ilan.zilcha@gmail.com>
2019-06-05 10:15:43 +0300

    On branch master  Changes to be committed: 	new file:   index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title> This is my first app <title>
+  </head>
+  <body>
+    <p> Hello world This is my first app</p>
+  </body>
+
+  <footer>
+      <p> This is the footer</p>
+  </footer>
+
+</html>
